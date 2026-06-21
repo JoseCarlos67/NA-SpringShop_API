65b8ba78a4a29eded9257ed071c82f302a649a0a
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-06-21 16:17:33 -0300

    feat: OrderItem, manu-tomany association with extra atrributes

--- src/main/java/com/jcarlos67/SpringShop_API/config/TestConfig.java
+++ src/main/java/com/jcarlos67/SpringShop_API/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.jcarlos67.SpringShop_API.config;
 
-import com.jcarlos67.SpringShop_API.entities.Category;
-import com.jcarlos67.SpringShop_API.entities.Order;
-import com.jcarlos67.SpringShop_API.entities.Product;
-import com.jcarlos67.SpringShop_API.entities.User;
+import com.jcarlos67.SpringShop_API.entities.*;
 import com.jcarlos67.SpringShop_API.entities.enums.OrderStatus;
-import com.jcarlos67.SpringShop_API.repositories.CategoryRepository;
-import com.jcarlos67.SpringShop_API.repositories.OrderRepository;
-import com.jcarlos67.SpringShop_API.repositories.ProductRepository;
-import com.jcarlos67.SpringShop_API.repositories.UserRepository;
+import com.jcarlos67.SpringShop_API.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -34,37 +28,49 @@ public class TestConfig implements CommandLineRunner {
   @Autowired
   private ProductRepository productRepository;
 
+  @Autowired
+  private OrderItemRepository orderItemRepository;
+
   @Override
   public void run(String... args) throws Exception {
-    User user1 = new User("Maria Brown", "maria@gmail.com", "99999999", "2432423");
-    User user2 = new User("Larissa B.", "larissa@gmail.com", "9999999", "34893082904");
 
-    Order order1 = new Order(Instant.parse("2025-06-20T19:45:08Z"), OrderStatus.WAITING_PAYMENT, user1);
-    Order order2 = new Order(Instant.parse("2025-06-20T09:10:45Z"), OrderStatus.PAID, user2);
+    Category cat1 = new Category( "Electronics");
+    Category cat2 = new Category( "Books");
+    Category cat3 = new Category( "Computers");
 
-    Category cat1 = new Category("Electronics");
-    Category cat2 = new Category("Books");
-    Category cat3 = new Category("Computers");
-
-    categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-
-    userRepository.saveAll(Arrays.asList(user1, user2));
-    orderRepository.saveAll(Arrays.asList(order1, order2));
-
-    Product p1 = new Product("The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+    Product p1 = new Product( "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
     Product p2 = new Product( "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
     Product p3 = new Product( "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
     Product p4 = new Product( "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
     Product p5 = new Product( "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
+    categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
     p1.getCategories().add(cat2);
-    p2.getCategories().addAll(List.of(cat1, cat3));
+    p2.getCategories().add(cat1);
+    p2.getCategories().add(cat3);
     p3.getCategories().add(cat3);
     p4.getCategories().add(cat3);
     p5.getCategories().add(cat2);
 
     productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+    User u1 = new User( "Maria Brown", "maria@gmail.com", "988888888", "123456");
+    User u2 = new User( "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+    Order o1 = new Order( Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+    Order o2 = new Order( Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+    Order o3 = new Order( Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
+    userRepository.saveAll(Arrays.asList(u1, u2));
+    orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+    OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+    OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+    OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+    OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+    orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
   }
 }
--- src/main/java/com/jcarlos67/SpringShop_API/entities/Product.java
+++ src/main/java/com/jcarlos67/SpringShop_API/entities/Product.java
@@ -2,13 +2,15 @@ package com.jcarlos67.SpringShop_API.entities;
 
 import jakarta.persistence.*;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb_product")
-public class Product {
+public class Product implements Serializable {
+  private static final long serialVersionUID = 1L;
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
