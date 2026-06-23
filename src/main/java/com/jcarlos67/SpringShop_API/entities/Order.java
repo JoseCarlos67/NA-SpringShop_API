6589c0f60316499f8a1bb344e94c0db9fc3de21f
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-06-23 18:45:15 -0300

    feat: subtotal e total methods

--- src/main/java/com/jcarlos67/SpringShop_API/entities/Order.java
+++ src/main/java/com/jcarlos67/SpringShop_API/entities/Order.java
@@ -86,6 +86,14 @@ public class Order implements Serializable {
     this.payment = payment;
   }
 
+  public Double getTotal() {
+    double sum = 0.0;
+    for (OrderItem x : items)
+      sum += x.getSubTotoal();
+
+    return sum;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (o == null || getClass() != o.getClass()) return false;
