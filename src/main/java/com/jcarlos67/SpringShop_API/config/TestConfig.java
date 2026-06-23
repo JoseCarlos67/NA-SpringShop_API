1f1a43689e6cb7142835e695436a528fd47ee04d
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-06-23 17:28:14 -0300

    feat: Product-OrderItem one-to-many association

--- src/main/java/com/jcarlos67/SpringShop_API/config/TestConfig.java
+++ src/main/java/com/jcarlos67/SpringShop_API/config/TestConfig.java
@@ -10,8 +10,6 @@ import org.springframework.context.annotation.Profile;
 
 import java.time.Instant;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
 
 @Configuration
 @Profile("test")
@@ -48,10 +46,14 @@ public class TestConfig implements CommandLineRunner {
     productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
     p1.getCategories().add(cat2);
+
     p2.getCategories().add(cat1);
     p2.getCategories().add(cat3);
+
     p3.getCategories().add(cat3);
+
     p4.getCategories().add(cat3);
+
     p5.getCategories().add(cat2);
 
     productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
